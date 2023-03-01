aded80f5e5b03701557e23f9c4c04276e508e6cb
spin0r <nils.bochow@gmail.com>
2023-03-01 17:17:23 +0100

    Update everything

--- submit_ecn_ensemble.sh
+++ submit_ecn_ensemble.sh
@@ -2,20 +2,21 @@
 
 #SBATCH --account=nn8008k  # Substitute with your project name
 #SBATCH --job-name=ecn_ensemble
-
+##SBATCH --qos=devel
 
 #SBATCH --time=1-20:00:00
+##SBATCH --time=00:30:00
 #SBATCH --nodes=1
 # Safety settings
 set -o errexit
 set -o nounset
-export JULIA_NUM_THREADS=16
+export JULIA_NUM_THREADS=32
 
 # Load MPI module
 module load Julia/1.7.1-linux-x86_64 
 
 
 
-srun --ntasks=16 --cpus-per-task=1 --account=nn8008k --time=12:00:00 -o ecn.out -e ecn.errr julia ensemble_ecn.jl & 
+srun --account=nn8008k --time=00:30:00 julia ensemble_ecn.jl &> ecn.out & 
 
 wait
